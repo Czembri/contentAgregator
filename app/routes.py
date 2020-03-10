8fa3dc24288dc5d348469e429a52dc821ca867d0
Aleksandra Czembrowska <czembri@gmail.com>
2020-03-10 21:18:14 +0100

    win update

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from app import app, db
 import requests
 from bs4 import BeautifulSoup
-
 
 routes = Blueprint("routes", __name__, static_folder="static", template_folder="templates")
 
@@ -13,21 +12,40 @@ def home():
 @app.route('/google')
 def google():
     url_goo = 'https://news.google.com/?hl=en-US&gl=US&ceid=US:en'
-    response_goo = requests.get(url).text
+    response_goo = requests.get(url_goo).text
     soup_goo = BeautifulSoup(response_goo, 'html.parser')
     gc = []
-    for content_goo in soup_goo.find_all('a', class_='DY5T1d'):
+    for content_goo in soup_goo.find_all('a', class_='DY5T1d')[0:5]:
         google_content = content_goo.getText().split(',')
         gc.append(google_content)
     return render_template('google.html', gc=gc)
 
-@app.route('/reddit')
-def reddit():
-    url_rd = 'https://www.reddit.com'
-    response_rd = requests.get(url_rd).text
-    soup_rd = BeautifulSoup(response_rd, 'html.parser')
-    rd = []
-    for content_rd in soup_rd.find_all('div', class_='_2SdHzo12ISmrC8H86TgSCp'):
-        reddit_content = content_rd.getText().split(',')
-        rd.append(reddit_content)
-    return render_template('reddit.html', rd=rd)
+@app.route('/bbc')
+def bbc():
+    url_bbc = 'https://www.bbc.com/news'
+    response_bbc = requests.get(url_bbc).text
+    soup_bbc = BeautifulSoup(response_bbc, 'html.parser')
+    bbc = []
+    for content_bbc in soup_bbc.find_all('h3')[1:6]:
+        bbc_content = content_bbc.getText().split(',')
+        bbc.append(bbc_content)
+    return render_template('bbc.html', bbc=bbc)
+
+@app.route('/cnn')
+def cnn():
+    url_cnn = 'https://edition.cnn.com/world'
+    response_cnn = requests.get(url_cnn).text
+    soup_cnn = BeautifulSoup(response_cnn, 'html.parser')
+    cnn = []
+    for content_cnn in soup_cnn.find_all('span', class_='cd__headline-text')[0:5]:
+        cnn_content = content_cnn.getText().split(',')
+        cnn.append(cnn_content)
+    for links in soup_cnn.find_all('a')[0:5]:
+        link_1 = soup_cnn.find('div', class_="cd__content").a.get('href')
+
+    return render_template('cnn.html', cnn=cnn, link=link_1)
+
+@app.route('/test')
+def test():
+    flash('Site under construction!', 'danger')
+    return render_template('test.html')
