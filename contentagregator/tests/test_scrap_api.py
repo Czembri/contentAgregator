4aeb1acfbfe7eacd37c41f2fa4b19aebd2acd768
Czembri <czembri@gmail.com>
2020-12-24 17:37:18 +0100

    news get api endpoints - tests

---
+++ contentagregator/tests/test_scrap_api.py
@@ -0,0 +1,13 @@
+from contentagregator.tests.base import BaseTestCase
+
+class TestErrorTemplates(BaseTestCase):
+    # Ensure id is correct for the current/logged in user
+    def test_404(self):
+        with self.client:
+            self.client.get('/undefined', follow_redirects=True)
+            self.assert404
+
+
+
+if __name__ == '__main__':
+    unittest.main()
