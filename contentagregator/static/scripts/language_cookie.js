e1b62a52177e6762b14df5df67c1e06722e52e54
Czembri <czembri@gmail.com>
2021-01-28 18:55:28 +0100

    setup forum index translations

--- contentagregator/static/scripts/language_cookie.js
+++ contentagregator/static/scripts/language_cookie.js
@@ -14,10 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     enButton.addEventListener("click", function(){
         setLangCookie("en");
+        location.reload();
     });
 
     plButton.addEventListener("click", function(){
         setLangCookie("pl");
+        location.reload();
     });
 
     var lang = getCookie("user_lang");
@@ -31,14 +33,25 @@ document.addEventListener("DOMContentLoaded", () => {
     function translate(lang){
       if ($("#lang").text() =='Polish'){
           $.get('/api/translations', function (data) {
-            for (let t of data){
-                $('.t-user').text(t['forum-create'][0]['Username']);
-                $('.t-group').text(t['forum-create'][0]['Group']);
-                $('.t-title').text(t['forum-create'][0]['Title']);
-                $('.t-content').text(t['forum-create'][0]['Content']);
-                $('.t-attach').text(t['forum-create'][0]['AddAttachment']);
-                $('.t-btn').text(t['forum-create'][0]['Addpost']);
-            }
+              if (typeof markPage !== 'undefined'){
+                for (let t of data){
+                  if (markPage == "create"){
+                    
+                        $('.t-user').text(t['forum-create'][0]['Username']);
+                        $('.t-group').text(t['forum-create'][0]['Group']);
+                        $('.t-title').text(t['forum-create'][0]['Title']);
+                        $('.t-content').text(t['forum-create'][0]['Content']);
+                        $('.t-attach').text(t['forum-create'][0]['AddAttachment']);
+                        $('.t-btn').text(t['forum-create'][0]['Addpost']);
+                    
+                  } else if (markPage == "index"){
+                    $('.write-post').text(t['forum-index'][0]['Writeapost']);
+                    $('.explore').text(t['forum-index'][0]['Explore']);
+                    $('.go-to').text(t['forum-index'][0]['GoTo']);
+                  }
+                }
+              }
+            
           })
       }   
     }
