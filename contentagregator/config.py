2ce4ad64226004b71d322a1f237e573d9b1d6a28
Czembri <czembri@gmail.com>
2021-01-29 13:45:17 +0100

    flask env setup

--- contentagregator/config.py
+++ contentagregator/config.py
@@ -101,7 +101,8 @@ class DevelopmentConfig(Config):
 
 
 class TestConfig(Config):
-    SECRET_KEY = os.environ.get('FLASK_APP', 'run.py')
+    ENV = 'testing'
+    FLASK_APP = os.environ.get('FLASK_APP', 'run.py')
     SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{url}/{db}'.format(
     user=db_config['login'], password=db_config['password'], url=db_config['url'], db='redactorzone_test')
     DEBUG = True
