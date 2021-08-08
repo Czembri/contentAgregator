233c1ffe9850ab07a7c6d4f4227af7c88363a026
Aleksandra Czembrowska <aleksandra.czembrowska@opennet.pl>
2021-08-08 15:31:59 +0200

    ssl certificates clean up

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from contentagregator import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context='adhoc')
